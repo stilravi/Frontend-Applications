f75ad1297d8f4b3379bc08a4f50f35a38d3bd04c
Ravi Stil <ravi.raaf@gmail.com>
2021-11-22 16:23:38 +0100

    Removed standard app js content

---
+++ react-app/src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+function Title() {
+  return (
+    <header>
+      <h2>Title toch</h2>
+    </header>
+  );
+}
+
+export default Title;
---
+++ react-app/src/components/Title/Title.js
@@ -0,0 +1,11 @@
+import "./Title.css";
+
+function Title() {
+  return (
+    <div>
+      <h1>Hoi</h1>
+    </div>
+  );
+}
+
+export default Title;
